7f6592e458a4d40e88b1dca9df77c0ab05257c9b
Bryan.Jaramillo <bryanjaramillobaldeon@gmail.com>
2023-04-10 01:57:42 +0100

    refactor and fix karma config

--- karma.conf.js
+++ karma.conf.js
@@ -3,7 +3,7 @@
 
 module.exports = function (config) {
   config.set({
-    basePath: 'skull-king-counter',
+    basePath: '',
     frameworks: ['jasmine', '@angular-devkit/build-angular'],
     plugins: [
       require('karma-jasmine'),
