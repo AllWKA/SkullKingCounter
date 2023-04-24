4045e470afe6b71cd3e6170b734eca832d3cc96a
Bryan.Jaramillo <bryanjaramillobaldeon@gmail.com>
2023-04-24 02:37:58 +0100

    redirect to player form and create error message for invalid number of players

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,13 +1,44 @@
-import { Component } from '@angular/core';
-import { IonicModule } from '@ionic/angular';
+import {Component} from '@angular/core'
+import {IonicModule} from '@ionic/angular'
+import {Router} from '@angular/router'
+import {NgIf} from '@angular/common'
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
   standalone: true,
-  imports: [IonicModule],
+  imports: [IonicModule, NgIf],
 })
 export class HomePage {
-  constructor() {}
+  numPlayers = 2
+
+  audio = new Audio()
+
+  wrongNumPlayers = false
+
+  constructor(private router: Router) {
+  }
+
+  async ngOnInit() {
+    this.audio.src = "/assets/sounds/menu.mp3"
+
+    this.audio.load()
+
+    this.audio.play()
+  }
+
+  start() {
+    if (this.numPlayers > 2 && this.numPlayers <= 6) {
+      this.audio.pause()
+
+      this.router.navigate(['player-form', this.numPlayers])
+    } else {
+      this.wrongNumPlayers = true
+    }
+  }
+
+  updateNumPlayers($event: any) {
+    this.numPlayers = $event.target.value
+  }
 }
